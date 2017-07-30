f32d838a1d5b205e974110523d387728890f9e94
Robin Hoens <robin.hoens@gmail.com>
2017-07-30 22:24:42 +0200

    Initial version

---
+++ vorwahl/Vorwahl.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace vorwahl
+{
+    public enum NummerTyp
+    {
+        Fixed,
+        Mobile,
+        Special,
+        Unknown
+    };
+
+    public class Vorwahl
+    {
+        private List<CountryCode> countryOrdering;
+
+        public Vorwahl()
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer ser = new System.Web.Script.Serialization.JavaScriptSerializer();
+            data = ser.Deserialize<VorwahlData>(VorwahlData.vorwahlJson);
+            countryOrdering = new List<CountryCode>
+            {
+                data.cc49,
+                data.cc41,
+                data.cc43,
+                data.cc33,
+                data.cc44,
+                data.cc1
+            };
+        }
+
+        public string Normalize(string nummer)
+        {
+            return Analyze(nummer)?.Item2;
+        }
+
+        private string TestRegex(string nummerNurZiffern, CountryCode country, Regex r)
+        {
+            Match m = r.Match(nummerNurZiffern);
+            if (m.Success && m.Groups.Count >= 4)
+            {
+                //                return m.Groups.Cast<Group>().Select(x => x.Value).Aggregate((a, b) => a + " " + b);
+                if (country.prefix.Contains("49"))
+                {
+                    return String.Concat(country.national, m.Groups[2].Value, " ", m.Groups[3].Value);
+                }
+                else
+                {
+                    string international = country.prefix.Substring(
+                        country.prefix.IndexOf('+'),
+                        country.prefix.IndexOf('|') - country.prefix.IndexOf('+')
+                        );
+
+                    return String.Concat(international, " ", m.Groups[2].Value, " ", m.Groups[3].Value);
+                }
+            }
+
+            return null;
+        }
+
+        private Tuple<NummerTyp, string> AnalyzeCountry(string nummerNurZiffern, CountryCode country)
+        {
+            string s = TestRegex(nummerNurZiffern, country, new Regex("^" + country.prefix + country.@fixed + @"(?:-)?(\d[-\d]*)"));
+            if (s != null)
+            {
+                return new Tuple<NummerTyp, string>(NummerTyp.Fixed, s);
+            }
+
+            s = TestRegex(nummerNurZiffern, country, new Regex("^" + country.prefix + country.mobile + @"(?:-)?(\d[-\d]*)"));
+            if (s != null)
+            {
+                return new Tuple<NummerTyp, string>(NummerTyp.Mobile, s);
+            }
+
+            s = TestRegex(nummerNurZiffern, country, new Regex("^" + country.prefix + country.special + @"(?:-)?(\d[-\d]*)"));
+            if (s != null)
+            {
+                return new Tuple<NummerTyp, string>(NummerTyp.Special, s);
+            }
+
+            return null;
+        }
+
+        public Tuple<NummerTyp, string> Analyze(string nummer)
+        {
+            string nummerNurZiffern = Regex.Replace(nummer, @"[^+\-0-9]", "");
+
+            foreach (var country in countryOrdering)
+            {
+                var resultCountry = AnalyzeCountry(nummerNurZiffern, country);
+                if (resultCountry != null)
+                {
+                    return resultCountry;
+                }
+            }
+
+            var r = new Regex("^" + data.international.prefix + data.international.cc + @"(?:-)?(\d[-\d]*)");
+            var m = r.Match(nummerNurZiffern);
+            if (m.Success && m.Groups.Count >= 4)
+            {
+                return new Tuple<NummerTyp, string>(NummerTyp.Unknown,
+                    String.Concat("+", m.Groups[2].Value, " ", m.Groups[3].Value));
+            }
+
+            return new Tuple<NummerTyp, string>(NummerTyp.Unknown, nummerNurZiffern);
+        }
+
+        private VorwahlData data;
+
+    }
+}
---
+++ vorwahltest/UnitTest1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace vorwahltest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void VorwahlFinden()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual("02129 58369", v.Normalize("0212958369"));
+        }
+
+        [TestMethod]
+        public void Schrott()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            var ergebnis = v.Analyze("bufgihwiuf");
+            Assert.AreEqual(vorwahl.NummerTyp.Unknown, ergebnis.Item1);
+            Assert.AreEqual("", ergebnis.Item2);
+        }
+
+        [TestMethod]
+        public void MitSonderzeichen()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual("02129 58369", v.Normalize("(02129) 58369"));
+        }
+
+        [TestMethod]
+        public void MitLaendervorwahl()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual("02129 58369", v.Normalize("+49 (2129) 58369"));
+        }
+
+        [TestMethod]
+        public void MitLaendervorwahl00()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual("02129 58369", v.Normalize("0049 2129 58369"));
+        }
+
+        [TestMethod]
+        public void MitLaendervorwahlKlammerNull()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual("0711 12345", v.Normalize("+49 (0)711-12345"));
+        }
+
+        [TestMethod]
+        public void MitFirmendurchwahl()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual("0211 94842-30", v.Normalize("0211-94842-30"));
+        }
+
+        [TestMethod]
+        public void International()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual("+593 32415406", v.Normalize("0059332415406"));
+        }
+
+        [TestMethod]
+        public void AnalysiereFixed()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            var ergebnis = v.Analyze("0212958369");
+            Assert.AreEqual(vorwahl.NummerTyp.Fixed, ergebnis.Item1);
+            Assert.AreEqual("02129 58369", ergebnis.Item2);
+        }
+
+        [TestMethod]
+        public void AnalysiereMobil()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            var ergebnis = v.Analyze("(0171)8765432");
+            Assert.AreEqual(vorwahl.NummerTyp.Mobile, ergebnis.Item1);
+            Assert.AreEqual("0171 8765432", ergebnis.Item2);
+        }
+
+        [TestMethod]
+        public void AnalysiereSpezial()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            var ergebnis = v.Analyze("01370668");
+            Assert.AreEqual(vorwahl.NummerTyp.Special, ergebnis.Item1);
+            Assert.AreEqual("0137 0668", ergebnis.Item2);
+        }
+
+        [TestMethod]
+        public void AnalysiereSchweiz()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            var ergebnis = v.Analyze("+41 44 412 11 11");
+            Assert.AreEqual(vorwahl.NummerTyp.Fixed, ergebnis.Item1);
+            Assert.AreEqual("+41 44412 1111", ergebnis.Item2);
+        }
+
+        [TestMethod]
+        public void AnalysiereSchweizMobil()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual(new Tuple<vorwahl.NummerTyp, string>(vorwahl.NummerTyp.Mobile, "+41 76378 2737"),
+                v.Analyze("+41 76 378 27 37"));
+        }
+
+        [TestMethod]
+        public void AnalysiereRumaenien()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual(new Tuple<vorwahl.NummerTyp, string>(vorwahl.NummerTyp.Unknown, "+40 364107375"),
+                v.Analyze("0040364/107375 (Rumänien)"));
+        }
+
+
+        [TestMethod]
+        public void AnalysiereOstatt0()
+        {
+            vorwahl.Vorwahl v = new vorwahl.Vorwahl();
+            Assert.AreEqual(new Tuple<vorwahl.NummerTyp, string>(vorwahl.NummerTyp.Fixed, "0711 765432"),
+                v.Analyze("O711765432"));
+        }
+    }
+}
